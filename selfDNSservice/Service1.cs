c1ce2cab9f5ae2234848bd5f617f7e5372d8ab17
kogdrix <kyrat.wow@gmail.com>
2022-10-12 10:01:28 +0200

    updated gethostentry, removed unnecessary logging, cleanup

--- selfDNSservice/Service1.cs
+++ selfDNSservice/Service1.cs
@@ -17,6 +17,9 @@ namespace selfDNSservice
     public partial class Service1 : ServiceBase
     {
         private static string selfDNS = "192.168.1.100";
+        private static int maxRetries = 3;
+
+
         private static int counter = 0;
         private static Timer timer = new Timer();
         private static EventLog eventLog1;
@@ -28,134 +31,91 @@ namespace selfDNSservice
             this.AutoLog = false;
             if (!System.Diagnostics.EventLog.SourceExists("DNSchanger"))
             {
-                System.Diagnostics.EventLog.CreateEventSource(
-                    "DNSchanger", "Log");
+                System.Diagnostics.EventLog.CreateEventSource("DNSchanger", "Log");
             }
+
             // configure the event log instance to use this source name
             eventLog1.Source = "DNSchanger";
             eventLog1.Log = "Log";
-            if (File.Exists(@"C:\\DNS\debug.txt"))
-            {
-                isDebug = true;
-            }
-            timer.Interval = 10000; // 6 seconds
+            timer.Interval = 5000; // 10 seconds
             timer.Elapsed += new ElapsedEventHandler(OnTimer);
             timer.Start();
         }
         private static void OnTimer(object sender, ElapsedEventArgs args)
         {
-            NetworkInterface currentInt = GetActiveEthernetOrWifiNetworkInterface();
-            bool workdns = false;
-            if (currentInt != null)
+            NetworkInterface currentInterfaces = GetActiveEthernetOrWifiNetworkInterface();
+            bool isDnsWorks = false;
+            if (currentInterfaces != null)
             {
-                if (GetDnsAdress(currentInt).ToString() == selfDNS)
+                if (GetDnsAdress(currentInterfaces).ToString() == selfDNS)
                 {
                     try
                     {
-                        IPHostEntry hostInfo = Dns.GetHostByName("www.google.com");
-                        workdns = true;
-
+                        // can resolve google.com
+                        IPHostEntry hostInfo = Dns.GetHostEntry("www.google.com");
+                        isDnsWorks = true;
                     }
                     catch (Exception)
                     {
-
-
                     }
-                    if ((!workdns) || (!PingHost(selfDNS)))
+
+                    // If dns doesn't work, and cant ping the server tries until the given retry number is reached.
+                    if ((!isDnsWorks) || (!PingHost(selfDNS)))
                     {
                         if (IsthereNet())
                         {
-                            Write(counter.ToString() + ". try - Cant ping self dns server!");
                             counter++;
                         }
                         else
                         {
-                            Write("Cant resolve or ping, no internet");
                         }
-
                     }
-                    if (counter == 3)
+                    if (counter == maxRetries)
                     {
                         counter = 0;
-                        Write("Reset DNS to 8.8.8.8, set recheck to 60 sec", true);
                         eventLog1.WriteEntry("Reset to google DNS");
                         SetDNS("8.8.8.8");
-                        timer.Interval = 60000;
+                        timer.Interval = 60000; // if had to set dns to new, set check to 60 sec.
                     }
                 }
                 else
                 {
                     if (PingHost(selfDNS))
                     {
-                        Write("Set Self DNS", true);
                         SetDNS(selfDNS);
                         eventLog1.WriteEntry("Set Self DNS server");
-
-                       timer.Interval = 20000;
+                        timer.Interval = 20000;
                     }
                     else
                     {
-                        Write("Cant ping self dns, cant set self back");
+                        eventLog1.WriteEntry("Cant ping self dns, cant set self back");
                     }
                 }
             }
             else
             {
-                Write("No active interface, no actions");
+                eventLog1.WriteEntry("No active interface, no actions");
             }
-
-
-
-
         }
+
         protected override void OnStart(string[] args)
         {
-            Write("DNS Changer started", true);
             eventLog1.WriteEntry("DNS Changer started");
         }
 
         protected override void OnStop()
         {
-            Write("DNS Changer stopped", true);
             eventLog1.WriteEntry("DNS Changer stopped");
         }
-
-
-        private static void Write(string inp, bool defdeb = false)
-        {
-            if (isDebug)
-            {
-                //Console.WriteLine(DateTime.Now + " | " + inp);
-                
-                using (StreamWriter w = File.AppendText(@"C:\\DNS\log.txt"))
-                {
-                    w.WriteLine(DateTime.Now + " | " + inp);
-                }
-            }
-            else
-            {
-                if (defdeb)
-                {
-                    using (StreamWriter w = File.AppendText(@"C:\\DNS\log.txt"))
-                    {
-                        w.WriteLine(DateTime.Now + " | " + inp);
-                    }
-                }
-            }
-           
-        }
-
       
         private static bool IsthereNet()
         {
             if (!PingHost("8.8.8.8") && !PingHost("4.2.2.1") && !PingHost("1.1.1.1"))
             {
-                Write("Theres is no internet");
                 return false;
             }
             else
             {
-                Write("Theres is internet");
                 return true;
             }
         }
@@ -169,6 +129,7 @@ namespace selfDNSservice
 
             return Nic;
         }
+
         public static void SetDNS(string DnsString)
         {
 
@@ -214,8 +175,8 @@ namespace selfDNSservice
                     }
                 }
             }
-
         }
+
         private static IPAddress GetDnsAdress(NetworkInterface networkInterface)
         {
             if (networkInterface.OperationalStatus == OperationalStatus.Up)
@@ -228,6 +189,7 @@ namespace selfDNSservice
                     return dnsAdress;
                 }
             }
+
             throw new InvalidOperationException("Unable to find DNS Address");
         }
 
@@ -243,7 +205,6 @@ namespace selfDNSservice
                 pinger = new Ping();
                 PingReply reply = pinger.Send(nameOrAddress, 500);
                 pingable = reply.Status == IPStatus.Success;
-                Write("Pingtry: " + nameOrAddress);
             }
             catch (PingException)
             {
@@ -259,11 +220,5 @@ namespace selfDNSservice
 
             return pingable;
         }
-
-
-
-
-
-
     }
 }
